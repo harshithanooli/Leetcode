8d4880c9b47346b4240e11c71bba99c75e092ed3
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-03-02 19:26:04 -0500

    Time: 0 ms (100%), Space: 40.7 MB (35.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,29 +1,26 @@
 class Solution {
 
-    public int paths(int[][] dp,int i, int j){
-        if(i==0 && j==0){
-            return dp[0][0]=1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        int up=paths(dp, i-1, j);
-        int left=paths(dp, i, j-1);
-
-        return dp[i][j]=up+left;
-    }
+    
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0, left=0;
+                if(i>0){
+                   up =dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+                }
+            }
         }
 
-
-
-        return paths(dp,m-1,n-1);
+        return dp[m-1][n-1];
     }
 }
