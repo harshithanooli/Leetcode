5fd02d45f21e87c9d54629530d2fdbb027fdb751
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-03-04 17:50:14 -0500

    Time: 5 ms (34.7%), Space: 44.2 MB (53.95%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int f(List<List<Integer>> triangle, int i, int j, int[][] dp){
-        if(i==triangle.size()-1){
-            return dp[i][j]=triangle.get(i).get(j);
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int straight=triangle.get(i).get(j)+f(triangle,(i+1),(j),dp);
-        int diagonal=triangle.get(i).get(j)+f(triangle,(i+1),(j+1),dp);
-
-        return dp[i][j]=Math.min(straight, diagonal);
-    }
+    
     public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
         int[][] dp=new int[triangle.size()][triangle.size()];
-        for(int i=0;i<triangle.size();i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle.get(n-1).get(i);
         }
-        return f(triangle, 0,0,dp);
+
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int straight=triangle.get(i).get(j)+dp[i+1][j];
+                int diagonal=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(straight, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
