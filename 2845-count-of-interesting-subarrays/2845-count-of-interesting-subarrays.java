2ad050e7a57f11f8e9e06cff165f4276911b9c6d
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-04-26 21:55:24 -0400

    Time: 45 ms (6.47%), Space: 60.6 MB (52.07%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.java
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.java
@@ -1,18 +1,13 @@
 class Solution {
     public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
-        if(modulo==1 && k==0){
-            return (long)((nums.size()*(nums.size()+1))/2);
-        }
+        
         HashMap<Long, Long> hm=new HashMap<>();
         long res=0;
-        long pSum=nums.get(0)%modulo==k?1:0;
+        long pSum=0;
         hm.put(0L,1L);
-        hm.put(pSum,1L);
-        if(pSum%modulo==k){
-            res=res+1;
-        }
+        
 
-        for(int i=1;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             if(nums.get(i)%modulo==k){
                 pSum=pSum+1;
             }
