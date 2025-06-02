dc7abaa0bb979607d6a429bb0cb9240f0bb90c84
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-06-02 00:01:01 -0400

    Time: 13 ms (8.1%), Space: 46 MB (45.81%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+
+        Arrays.fill(candies, 1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        int res=0;
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+                candies[i-1] = Math.max(candies[i-1], candies[i]+1);
+            }
+            res=res+candies[i];
+        }
+        res=res+candies[0];
+        System.out.println(Arrays.toString(candies));
+        return res;
+        
+    }
+}
