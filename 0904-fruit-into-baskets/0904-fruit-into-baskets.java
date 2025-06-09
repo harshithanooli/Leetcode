9122b472899a7247d0e9c9e98909662f3567074b
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-06-08 20:36:32 -0400

    Time: 57 ms (10.88%), Space: 55.3 MB (52.47%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -2,15 +2,18 @@ class Solution {
     public int totalFruit(int[] fruits) {
         int res=0, l=0, r=0;
         int n=fruits.length;
-        HashSet<Integer> hs=new HashSet<>();
+        HashMap<Integer, Integer> hm=new HashMap<>();
         while(r<n){
-            hs.add(fruits[r]);
-            if(hs.size()>2 && hs.contains(fruits[l])){
-                    hs.remove(fruits[l]);
+            hm.put(fruits[r], hm.getOrDefault(fruits[r],0)+1);
+            if(hm.size()>2 && hm.containsKey(fruits[l])){
+                    hm.put(fruits[l], hm.get(fruits[l])-1);
+                    if(hm.get(fruits[l])<=0){
+                        hm.remove(fruits[l]);
+                    }
                     l++;
             }
 
-            if(hs.size()<=2){
+            if(hm.size()<=2){
                 res=Math.max(res, r-l+1);
             }
             r++;
