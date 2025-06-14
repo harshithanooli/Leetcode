619dd803d942a460e155ddaab4d6e29f60b6b15a
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-06-14 19:31:07 -0400

    Time: 23 ms (27.84%), Space: 45.9 MB (8.4%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,44 +1,42 @@
 class Solution {
 
-    public boolean check(HashMap<Character, Integer> sMap, HashMap<Character, Integer> tMap){
-        for(Character ch:tMap.keySet()){
-            if(!sMap.containsKey(ch) || sMap.get(ch)<tMap.get(ch))
-                return false;
-        }
-        return true;
-    }
+   
     public String minWindow(String s, String t) {
-        if(s.equals(t))
-        return s;
-        else if (s.length()<t.length()){
+        if(s.length()<t.length()){
             return "";
         }
-        String res="";
-        HashMap<Character, Integer> sMap=new HashMap<>();
-        HashMap<Character, Integer> tMap = new HashMap<>();
-        int tLength = t.length(), sLength = s.length();
-        for(int i=0;i<tLength;i++){
-            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0)+1);
+        int res=Integer.MAX_VALUE, count=0;
+        HashMap<Character, Integer> hmt=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            hmt.put(t.charAt(i), hmt.getOrDefault(t.charAt(i),0)+1);
         }
-        System.out.println(tMap);
-        int l=0;
-        for(int i=0;i<sLength;i++){
-            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i),0)+1);
-
-            while(l<i && check(sMap, tMap)){
-                if(res.equals("") || i-l+1< res.length()){
-                    res=s.substring(l, i+1);
+        int l=0, r=0;
+        HashMap<Character, Integer> hms=new HashMap<>();
+        int al=-1, ar=-1;
+        while(r<s.length()){
+            hms.put(s.charAt(r), hms.getOrDefault(s.charAt(r),0)+1);
+            if(hmt.containsKey(s.charAt(r)) && hms.get(s.charAt(r))<=hmt.get(s.charAt(r))){
+                count++;
+            }
+            while(count==t.length()){
+                // System.out.println("l:" +l+" r: "+r);
+                if(res>=(r-l+1)){
+                    res=(r-l+1);
+                    al=l;
+                    ar=r;
                 }
-                sMap.put(s.charAt(l), sMap.get(s.charAt(l))-1);
-                if(sMap.get(s.charAt(l))==0)
-                    sMap.remove(s.charAt(l));
+                hms.put(s.charAt(l), hms.getOrDefault(s.charAt(l), 0)-1);
+                if(hmt.containsKey(s.charAt(l)) && hms.get(s.charAt(l))<hmt.get(s.charAt(l))){
+                    count--;
+                }
                 l++;
             }
-            System.out.println("i : "+i+" l: "+l);
-            System.out.println("Res: "+res);
-            
+            r++;
         }
-
-        return res;
+        if(al==-1 || ar==-1){
+            return "";
+        }
+        // System.out.println("al : "+al+" ar: " +ar);
+        return s.substring(al, ar+1);
     }
 }
