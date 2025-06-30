69cada3e362ed452d3d55670bb7ea94bc7fa5f80
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-06-29 22:12:17 -0400

    Time: 3 ms (32.63%), Space: 47.4 MB (74.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,28 +1,56 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        boolean[] visited=new boolean[isConnected.length];
+    public void dfs(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited){
+       
+            visited[node]=true;
 
-        Stack<Integer> s=new Stack<>();
+            for(int adjNode : adjList.get(node)){
+                if(!visited[adjNode])
+                    dfs(adjNode, adjList, visited);
+            }
         
-        int res=0;
+
+        return;
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int res = 0;
+        boolean visited[]= new boolean[isConnected.length];
+        // Stack<Integer> s = new Stack<>();
+        // for(int i=0;i<isConnected.length;i++){
+        //     if(!visited[i]){
+        //         res++;
+        //         s.push(i);
+
+        //         while(!s.isEmpty()){
+        //             Integer node = s.pop();
+        //             visited[node]=true;
+
+        //             for(int j=0;j<isConnected[node].length;j++){
+        //                 if(isConnected[i][j]==1 && !visited[j]){
+        //                     s.push(j);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
+
         for(int i=0;i<isConnected.length;i++){
-            if(!visited[i]){
-
-                res++;
-                s.push(i);
-
-                while(!s.isEmpty()){
-                    int node = s.peek();
-                    s.pop();
-                    visited[node]=true;
-                    for(int j=0;j<isConnected[node].length;j++){
-                        if(isConnected[node][j]==1 && !visited[j]){
-                            s.push(j);
-                        }
-                    }
-                }
+            adjList.add(new ArrayList<Integer>());
+            for(int j=0;j<isConnected[i].length;j++){
+                if(isConnected[i][j]==1)
+                    adjList.get(i).add(j);
             }
         }
+
+        for(int i=0;i<adjList.size();i++){
+
+            if(!visited[i]){
+                res++;
+                dfs(i, adjList, visited);
+            }
+        }
+
         return res;
     }
 }
