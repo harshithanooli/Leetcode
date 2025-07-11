059be9a73d04fa195e90d2e68c11e069aed494b3
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-07-11 02:49:44 -0400

    Time: 6 ms (73.82%), Space: 45.9 MB (22.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -27,16 +27,48 @@ class Solution {
             adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
 
-        // Stack<Integer> st =new Stack<>();
-        boolean[] visited = new boolean[numCourses+1];
-        boolean[] pathVisited = new boolean[numCourses+1];
-        for(int i=0;i<=numCourses; i++){
-            if(!visited[i]){
-                if(dfs(visited, pathVisited, adjList, i))
-                    return false;
-            }           
+        // kahn's algorithm - bfs
+        int[] indegree = new int[numCourses];
+        int visited = 0;
+
+        for(int i=0;i<adjList.size();i++){
+            for(int adjNode: adjList.get(i)){
+                indegree[adjNode]++;
+            }
         }
 
-        return true;
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)
+                q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            visited++;
+
+            for(int adjNode : adjList.get(node)){
+                indegree[adjNode]--;
+                if(indegree[adjNode]==0){
+                    q.add(adjNode);
+                }
+            }
+        }
+
+        return visited == numCourses;
+
+        // toplogical sorting - dfs
+        // // Stack<Integer> st =new Stack<>();
+        // boolean[] visited = new boolean[numCourses+1];
+        // boolean[] pathVisited = new boolean[numCourses+1];
+        // for(int i=0;i<=numCourses; i++){
+        //     if(!visited[i]){
+        //         if(dfs(visited, pathVisited, adjList, i))
+        //             return false;
+        //     }           
+        // }
+
+        //return true;
     }
 }
