c1e87d0d7818c1c1b3d2874048d6bc1842462794
harshithanooli <60030580+harshithanooli@users.noreply.github.com>
2025-09-10 21:15:18 -0400

    Time: 63 ms (27.97%) | Memory: 47.6 MB (48.98%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -2,29 +2,26 @@ class Solution {
     public String sortVowels(String s) {
 
         ArrayList<Character> vowels = new ArrayList<>();
-        char[] vowel = {'a','e','i','o','u','A','E','I','O','U'};
+        Character[] chars = {'a','e','i','o','u','A','E','I','O','U'};
+        HashSet<Character> vowel=new HashSet<Character>(Arrays.asList(chars));
 
         for(char ch:s.toCharArray()){
-            for(char c:vowel){
-                if(c==ch){
+            
+                if(vowel.contains(ch)){
                     vowels.add(ch);
                 }
-            }    
+              
         } 
         int j=0;
         Collections.sort(vowels);
         StringBuilder sb=new StringBuilder("");
         for(int i=0;i<s.length();i++){
             boolean flag=false;
-            for(char c:vowel){
-                if(c==s.charAt(i)){
-                    sb.append(vowels.get(j));
-                    j++;
-                    flag=true;
-                    break;
-                }  
+            if (vowel.contains(s.charAt(i))){
+                sb.append(vowels.get(j));
+                j++;
             }
-            if(!flag){
+            else {
                 sb.append(s.charAt(i));
             }
         }
